911e1d41f1209865d23184dde0ede885af0955bb
marqueles <marcosjggarcia@gmail.com>
2018-06-24 14:30:29 +0200

    Exercise 2 first commit, EntityFrameworkCore used

---
+++ Exercise2/edX.DataApp.CoreConsole/edX.DataApp.CoreConsole/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace edX.DataApp.CoreConsole
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        public string Name { get; set; }
+
+        public string ProductNumber { get; set; }
+
+        public string Color { get; set; }
+
+        public decimal StandardCost { get; set; }
+
+        public decimal ListPrice { get; set; }
+    }
+}
---
+++ Exercise2/edX.DataApp.CoreConsole/edX.DataApp.CoreConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace edX.DataApp.CoreConsole
+{
+    class Program
+    {
+        static async Task RunAsync()
+        {
+            using(ContosoContext context = new ContosoContext())
+            {
+                var creator = context.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+                await creator.ExistsAsync();
+                Console.WriteLine("Connection Succesfull");
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            RunAsync().Wait();
+            Console.WriteLine("Application has completed execution. Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
